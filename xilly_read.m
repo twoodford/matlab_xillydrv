1434bc0a9352d660e252a316e364ee40ea70271f
Tim Woodford <timthewoodford@gmail.com>
2019-05-15 14:54:41 -0700

    Fix xilly_read

--- xilly_read.m
+++ xilly_read.m
@@ -1,5 +1,8 @@
 function read_data = xilly_read(streamfile, ctrlfile, num_samples)
 
+% 128-bit packets, 16-bit samples
+num_ofxfer = num_samples/128*16;
+
 % Set the number of samples
 xilly_memwrite(ctrlfile, 1, bitand(num_ofxfer,255));
 xilly_memwrite(ctrlfile, 2, bitand(bitshift(num_ofxfer,-8),255));
@@ -10,6 +13,6 @@ xilly_memwrite(ctrlfile, 4, bitand(bitshift(num_ofxfer,-24),255));
 xilly_memwrite(ctrlfile, 0, 1);
 
 % Grab samples
-read_data = xilly_fiforead(streamfile, num_samples)
+read_data = xilly_fiforead(streamfile, num_samples);
 
 end
