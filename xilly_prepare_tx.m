00530e673d916b3434039a8669ef270a4263386a
Tim Woodford <timthewoodford@gmail.com>
2019-05-30 13:53:56 -0700

    Fix register addresses in prepare_tx

--- xilly_prepare_tx.m
+++ xilly_prepare_tx.m
@@ -4,8 +4,8 @@ ctrlfile = '/dev/xillybus_mem_8';
 num_samples = length(tx_samples);
 mem_end_addr = num_samples/128*16;
 
-xilly_memwrite(ctrlfile, 1, bitand(mem_end_addr,255));
-xilly_memwrite(ctrlfile, 2, bitand(bitshift(mem_end_addr,-8),255));
+xilly_memwrite(ctrlfile, 5, bitand(mem_end_addr,255));
+xilly_memwrite(ctrlfile, 6, bitand(bitshift(mem_end_addr,-8),255));
 
 % Assume all samples are real for now, will change in the future
 fh = fopen('/dev/xillybus_mem_128', 'a');
